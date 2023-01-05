169e04a3429cb15fd2deb90c279ac98fa86e36ca
NadjaKh <Nadezhda.Kharitonova@yh.nackademin.se>
2023-01-05 09:46:33 +0100

    added second way to write function

--- function.js
+++ function.js
@@ -4,6 +4,13 @@
 - Om heltalet är 1-10 så returnerar den heltaled
 - Om heltalet är över 10 så returneras talet gånger sig själv*/
 
+const result = (heltalTest) => {
+    if(heltalTest<0){return 0}
+    else if (heltalTest>10)
+    {return heltalTest*heltalTest}
+    return heltalTest;
+}
+
 function funktion(a){
     if(a<0){return 0}
     else{if((a>0)&&(a<11))
@@ -11,10 +18,16 @@ function funktion(a){
     else{return a*a}}
 };
 heltal = -1;
+heltalTest = -1;
 console.log(`function for ${heltal} is ${funktion(heltal)}`);
+console.log(`result for ${heltalTest} is ${result(heltalTest)}`);
 
 heltal = 10;
+heltalTest = 10;
 console.log(`function for ${heltal} is ${funktion(heltal)}`);
+console.log(`result for ${heltalTest} is ${result(heltalTest)}`);
 
 heltal = 11;
+heltalTest = 11;
 console.log(`function for ${heltal} is ${funktion(heltal)}`);
+console.log(`result for ${heltalTest} is ${result(heltalTest)}`);
